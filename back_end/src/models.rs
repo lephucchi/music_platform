a2c2d4889f7f7bc14e0923fefd2cb4f7d136a354
unknown <phucchi12112004@gmail.com>
2025-04-18 11:26:23 +0700

    defined models for db

---
+++ back_end/src/models.rs
@@ -0,0 +1,111 @@
+use serde::{Deserialize, Serialize};
+
+
+#[Derive(Debug, Serialize , Deserialize , CLone)]
+pub struct Duration {
+    pub months: i32,
+    pub days: i32,
+    pub microseconds: i32,
+}
+
+impl Duration {
+    fn default() -> Self {
+        Self {
+            months: 0,
+            days: 0,
+            microseconds: 0,
+        }
+    }
+}
+
+impl From<PgInterval> for Duration {
+    fn from(interval: PgInterval) -> Self {
+        Self {
+            months: interval.months,
+            days: interval.days,
+            microseconds: interval.microseconds,
+        }
+    }
+}
+
+impl From<Option<PgInterval>> for Duration {
+    fn from(option: Option<PgInterval>) -> Self {
+        match option {
+            Some(interval) => Duration::from(interval),
+            None => Duration::default(),
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct User{
+    pub id: Uuid,
+    pub username: String,
+    pub email: String,
+    pub password_hash: String,
+    pub created_at: Option<NaiveDateTime>,
+    pub updated_at: Option<NaiveDateTime>, 
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Track {
+    pub id: Uuid,
+    pub user_id: Uuid,
+    pub title: Option<String>,
+    pub artist: Option<String>,
+    pub durations: Duration,
+    pub file_name: Option<String>,
+    pub upload_status: Option<String>,
+    pub thumbnail_name: Option<String>,
+    pub created_at: Option<NaiveDateTime>,
+    pub updated_at: Option<NaiveDateTime>
+}
+
+#[drive(Debug, Serialize, Deserialize, FromRow)]
+pub struct AudioFile {
+    pub id: Uuid,
+    pub track_id: Option<Uuid>,
+    pub total_chunks: i32,
+    pub upload_chunks: i32,
+    pub current_chunk: i32,
+    pub chunk_path: Option<String>,
+    pub upload_status: Option<String>,
+    pub created_at: Option<NaiveDateTime>,
+    pub updated_at: Option<NaiveDateTime>
+}
+
+
+#[drive(Debug, Serialize, Deserialize, FromRow)]
+pub struct PlaylistTrack {
+    pub playlist_id: Uuid,
+    pub track_id: Uuid,
+    pub track_oder: i32
+}
+
+
+#[drive(Debug, Serialize, Deserialize, FromRow)]
+pub struct PlaybackHistory {
+    pub id: Uuid,
+    pub user_id: Uuid,
+    pub track_id: Uuid,
+    pub played_at: NaiveDateTime,
+    pub duration_played: Duration
+
+}
+
+#[drive(Debug, Serialize, Deserialize, FromRow)]
+pub struct UserFavorite{
+    pub id: Uuid,
+    pub user_id: Uuid,
+    pub track_id: Uuid,
+    pub created_at: NaiveDateTime,
+}
+
+impl User {
+    pub fn new(name: String, email: String, password: String) -> Self {
+        Self { id: 0, name, email, password, created_at: Utc::now(), updated_at: Utc::now() }
+    }
+}
+
+
+
