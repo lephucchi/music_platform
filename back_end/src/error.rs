f9ae044ce8870f43b8bd855e8362f59f0915c092
unknown <phucchi12112004@gmail.com>
2025-04-19 23:47:52 +0700

    Erroro Handle basic

---
+++ back_end/src/error.rs
@@ -0,0 +1,22 @@
+use axum::{
+    http::StatusCode,
+    Response::{
+        IntoResponse,
+        Response,
+    },
+    Json,
+};
+use serde::{Deserialize,Serialize};
+use std::fmt;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct  ErrorResponse {
+    pub status:String,
+    pub message: String,
+}
+
+impl fmt::Display for ErrorResponse {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>)-> fmt::Result {
+        write!(f, "{}" , serde_json::to_string(&self).unwrap)
+    }
+}
