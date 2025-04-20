bd4d1d129be936ad5647d6a53e67da0d0f07ea23
unknown <phucchi12112004@gmail.com>
2025-04-20 16:15:05 +0700

    Comlete Errors handle and add Password hashing with argon2id

---
+++ back_end/src/errors.rs
@@ -0,0 +1,161 @@
+use axum::{
+    http::StatusCode,
+    Response::{
+        IntoResponse,
+        Response,
+    },
+    Json,
+};
+use serde::{Deserialize,Serialize};
+use std::fmt;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct  ErrorResponse {
+    pub status:String,
+    pub message: String,
+}
+
+impl fmt::Display for ErrorResponse {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>)-> fmt::Result {
+        write!(f, "{}" , serde_json::to_string(&self).unwrap)
+    }
+}
+
+#[derive(Debug, PartiaEq)]
+pub enum ErrorMessage {
+    EmptyPassword,
+    ExceededMaxPasswordLength(usize),
+    HashingError,
+    InvalidHashFormat,
+    InvalidToken,
+    WrongCrendentials,
+    EmailExist,
+    UsernameExist,
+    UserNoLongerExist,
+    TokenNotProvided,
+}
+
+impl ToString for ErrorMessage {
+    fn to_string(&self) -> String {
+        self.to_str().to_owned()
+    }
+}
+
+impl ErrorMessage {
+    fn to_str(&self) -> String {
+        match self {
+            ErrorMessage::WrongCrendentials => "Invalid username or password".to_string(),
+            ErrorMessage::EmailExist => "This Email already exists".to_string(),
+            ErrorMessage::UsernameExist => "This username already exists".to_string(),
+            ErrorMessage::UserNoLongerExist => "This user is no longer exist".to_string(),
+            ErrorMessage::EmptyPassword => "Please fill the password".to_string(),
+            ErrorMessage::HashingError => "Error while hashing the password".to_string(),
+            ErrorMessage::InvalidHashFormat => "Invalid password hash format".to_string(),
+            ErrorMessage::ExceededMaxPasswordLength(max_length) => format!("Password must not be more than {} characters" , max_length),
+            ErrorMessage::InvalidToken => "Authentication token is invalid or expired".to_string(),
+            ErrorMessage::TokenNotProvided => "You are not logged in, please provide token".to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct HttpError {
+    pub message: String,
+    pub status: StatusCode,
+}
+
+impl HttpError {
+    pub fn new(message: impl Into<String>, status: StatusCode) -> Self {
+        HttpError {
+            message: message.info(),
+            status,
+        }
+    }
+
+    pub fn server_error(message: impl Into<String>) -> Self {
+        HttpError {
+            message: message.info(),
+            status: StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+
+    pub fn bad_request(message: impl Into<String>) -> Self {
+        HttpError {
+            message: message.info(),
+            status: StatusCode::BAD_REQUEST,
+        }
+    }
+
+    pub fn server_error(message: impl Into<String>) -> Self {
+        HttpError {
+            message: message.into(),
+            status: StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+
+    pub fn bad_request(message: impl Into<String>) -> Self {
+        HttpError {
+            message: message.into(),
+            status: StatusCode::BAD_REQUEST,
+        }
+    }
+
+    pub fn unique_constraint_violation(message: impl Into<String>) -> Self {
+        HttpError {
+            message: message.into(),
+            status: StatusCode::CONFLICT,
+        }
+    }
+
+    pub fn unauthorized(message: impl Into<String>) -> Self {
+        HttpError {
+            message: message.into(),
+            status: StatusCode::UNAUTHORIZED,
+        }
+    }
+    pub fn server_error(message: impl Into<String>) -> Self {
+        HttpError {
+            message: message.into(),
+            status: StatusCode::INTERNAL_SERVER_ERROR,
+        }
+    }
+
+    pub fn bad_request(message: impl Into<String>) -> Self {
+        HttpError {
+            message: message.into(),
+            status: StatusCode::BAD_REQUEST,
+        }
+    }
+
+    pub fn unique_constraint_violation(message: impl Into<String>) -> Self {
+        HttpError {
+            message: message.into(),
+            status: StatusCode::CONFLICT,
+        }
+    }
+
+    pub fn into_http_response(self) -> Response {
+        HttpError {
+            message: self.message.clone(),
+            status: "fail".to_string(),
+        }
+        (self.status, json_response).into_response()
+    }
+
+}
+
+impl fmt::Display for HttpError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write! (
+            f, "HttpError: message= {} , status = {}" , self.message , self.status
+        )
+    }
+}
+
+impl std::error::Error for HttpError {}
+
+impl IntoResponse for HttpError {
+    fn into_response(self) -> Response {
+        self.into_http_response
+    }
+}
---
+++ back_end/src/utils/password.rs
@@ -0,0 +1,68 @@
+use agron2::{
+    password_hash::{
+        rand_core::OsRng,
+        PasswordHash,
+        PasswordHasher,
+        PasswordVerifier,
+        SaltString,
+    },
+    Argon2,
+    Algorithm,
+    Version,
+    Params,
+};
+
+use crate::errors::ErrorMessage;
+
+const MAX_PASSWORD_LENGTH: usize = 64;
+
+pub fn hash(password: impl Into<String>) -> Result<String, ErrorMessage> {
+    let password = password.into();
+
+    if password.is_empty(){
+        return Err(ErrorMessage::EmptyPassword);
+    }
+
+    if password.len() > MAX_PASSWORD_LENGTH {
+        return Err(ErrorMessage::ExceededMaxPasswordLength(MAX_PASSWORD_LENGTH));
+    }
+
+    let salt = SaltString::generate(&mut OsRng);
+
+    let argon2 = Argon2::new(
+        Algorithm::Argon2id, //Balance between security and performance
+        Version::V0x13,
+        Params::new(
+            15*1024, // Memory cost
+            2, // Time cost
+            1, // Threads
+            None, //Optional output length
+        ).map_err(|_| ErrorMessage::HashingError)?,
+    );
+
+    let hashed_password = argon2
+    .hash_password(password.as_bytes(), &salt)
+    .map.err(|_| ErrorMessage::HashingError)
+    .to_string();
+
+    Ok(hashed_password)
+
+}
+
+pub fn compare(password: &str, hashed_password: &str) -> Result<Bool , ErrorMessage> {
+    if password.is_empty(){
+        return Err(ErrorMessage::EmptyPassword);
+    }
+
+    if password.len() > MAX_PASSWORD_LENGTH{
+        return Err(ErrorMessage::ExceededMaxPasswordLength(MAX_PASSWORD_LENGTH));
+    }
+
+    let parsed_hash = PasswordHash::new(hashed_password).map_err(|_| ErrorMessage::InvalidHashFormat)?;
+
+    let password_matched = Argon2::default().verify_password(password.as_bytes() , &parsed_hash).map_or(false, |_| true);
+
+    Ok(password_matched)
+}
+
+
