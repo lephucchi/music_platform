63f7ed91697b57c234785bda368d8563ccc36c30
unknown <phucchi12112004@gmail.com>
2025-04-24 20:08:58 +0700

    complete auth handler

--- back_end/src/handler/auth.rs
+++ back_end/src/handler/auth.rs
@@ -65,3 +65,56 @@ pub async fn register(
         Err(e) => Err(HttpError::server_error(e.to_string()))
     }
 }
+
+pub fn login(
+    Extention(app_state): Extention<Arc<AppState>>,
+    Json(body):Json<LoginUserDto>,
+) -> Result<impl IntoResponse, HttpError> {
+    // Kiem tra input
+    body.validate().map_err(|e| HttpError::bad_request(e.to_string()))? ;
+
+    //Fetch user
+    let mut result = app_state.db_client.get_user(None, None, Some(&body.identifier)).await.map_err(|e| HttpError::server_error(e.to_string()))?;
+
+    if result.is_none(){
+        result = app_state.db_client
+        .get_user(None, Some(&body.identifier), None)
+        .await
+        .map_err(|e| HttpError::server_error(e.to_string()))?;
+    }
+
+    let user = result.ok_or(HttpError::bad_request(ErrorMessage::WrongCredentials.to_string()))?;
+
+    //compare password
+    let password_matches = password::compare(&body.password, &user.password_hash).map_err(|_| HttpError::bad_request(ErrorMessage::WrongCredentials.to_string()))?;
+
+    if password_matches {
+        //Create a JWT token
+        let token = token::create_token(
+            &user.id.to_string(),
+            &app_state.env.jwt_secret.as_bytes(),
+            app_state.env.jwt_maxage,
+        ).map_err(|e| HttpError::server_error(e.to_string))?;
+
+        let cookie_duration = time::Duration::minutes(app_state.env.jwt_maxage*60);
+        let cookie = Cookie::build(("token" , token.clone())).path("/").max_age(cookie_duration).http_only(true).build();
+
+        let filter_user = FilterUserDto::filter_user(&user);
+        //prepare response
+        let response = axum::response::Json(UserLoginResponseDto{
+            status : "success".to_string(),
+            user: filter_user,
+            token,
+        });
+        let mut headers = HeaderMap::new();
+        headers.append(header::SET_COOKIE, cookie.to_string().parse().unwrap());
+        
+        let mut response = response.into_response();
+        response.headers_mut().extend(headers);
+
+        Ok(response);
+    }
+    else {
+        Err(HttpError::bad_request(ErrorMessage::WrongCredentials.to_string()))
+    }
+}
