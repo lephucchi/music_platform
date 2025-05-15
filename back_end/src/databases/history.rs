888157824d4f69aadeef81a50f2218c49d8c2ec2
unknown <phucchi12112004@gmail.com>
2025-05-15 12:01:16 +0700

    Complete history hadler and fix some syntax misstake

--- back_end/src/databases/history.rs
+++ back_end/src/databases/history.rs
@@ -77,28 +77,29 @@ impl HistoryExt for DBClient {
         &self,
         user_id: Uuid,
     ) -> Result<Vec<TractDto>, sqlx::Error> {
-        let query = sqlx::query_as!(r#"
-            SELECT
-                t.id,
-                t.title,
-                t.artist,
-                t.duration,
-                ph.duration_played,
-                ph.played_at,
-                t.file_name,
-                t.upload_status,
-                t.thumbnail_name,
-                CASE WHEN uf.id IS NOT NULL THEN true ELSE false END AS is_favorite,
-                CASE WHEN t.user_id = $1 THEN true ELSE false END AS is_created_by_user
-            From
-                playback_history ph
-            JOIN
-                tracks t ON ph.track_id = t.id
-            LEFT JOIN
-                user_favorites uf ON uf.track_id = t.id AND uf.user_id = $1
-            WHERE
-                ph.user_id = $1
-        "#,
+        let query = sqlx::query_as!(
+            r#"
+                SELECT
+                    t.id,
+                    t.title,
+                    t.artist,
+                    t.duration,
+                    ph.duration_played,
+                    ph.played_at,
+                    t.file_name,
+                    t.upload_status,
+                    t.thumbnail_name,
+                    CASE WHEN uf.id IS NOT NULL THEN true ELSE false END AS is_favorite,
+                    CASE WHEN t.user_id = $1 THEN true ELSE false END AS is_created_by_user
+                From
+                    playback_history ph
+                JOIN
+                    tracks t ON ph.track_id = t.id
+                LEFT JOIN
+                    user_favorites uf ON uf.track_id = t.id AND uf.user_id = $1
+                WHERE
+                    ph.user_id = $1
+            "#,
         user_id,
         )
         .fetch_all(&self.pool)
